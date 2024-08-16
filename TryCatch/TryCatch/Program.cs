7dcf5bbb0223a69a7fd78a4c5bc09cd0103a3a52
Sefa Saraç <srcsfysf@gmail.com>
2024-08-16 22:17:26 +0300

    Add files via upload

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int kare = sayi * sayi;
+            Console.WriteLine("Girdiğiniz sayının karesi: " + kare);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+    }
+}
